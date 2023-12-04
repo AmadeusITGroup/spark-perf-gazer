aa3585e5477d407a2192a033acd35b36d220a623
Mauricio Jost <mauricio.jost@amadeus.com>
2023-12-04 20:22:18 +0100

    Improve SparkInternal.planToMetrics

--- src/main/scala/com/amadeus/sparklear/SparkInternal.scala
+++ src/main/scala/com/amadeus/sparklear/SparkInternal.scala
@@ -1,26 +1,34 @@
 package org.apache.spark.sql.execution.ui
 
-import org.apache.spark.sql.execution.{FileSourceScanExec, SparkPlan}
+import org.apache.spark.sql.execution.command.ExecutedCommandExec
 import org.apache.spark.sql.execution.datasources.v2.V2ExistingTableWriteExec
+import org.apache.spark.sql.execution.{DeserializeToObjectExec, FileSourceScanExec, InputAdapter, MapElementsExec, MapPartitionsExec, ProjectExec, SerializeFromObjectExec, SparkPlan, UnaryExecNode, WholeStageCodegenExec}
 import org.apache.spark.sql.execution.metric.SQLMetric
 
 object SparkInternal {
+
+  val Empty = Map.empty[Long, Long]
+
   def executedPlanMetrics(event: SparkListenerSQLExecutionEnd): Map[Long, Long] = {
-    val ep = event.qe.executedPlan
-    val metrics = ep match {
-      case i: V2ExistingTableWriteExec => resolvePlan(i.query)
-      case _ =>
-        throw new IllegalStateException(s"Not supported executedPlanMetrics(${ep.getClass.getName})")
-    }
-    metrics
+    planToMetrics(event.qe.executedPlan)
   }
 
-  private def resolvePlan(p: SparkPlan): Map[Long, Long] = {
+  private def planToMetrics(p: SparkPlan): Map[Long, Long] = {
     val node = p match {
-      case i: FileSourceScanExec => toMetric(i.driverMetrics) ++ toMetric(i.metrics)
-      case _ => throw new IllegalStateException(s"Not supported resolvePlan(${p.getClass.getName})")
+      case i: FileSourceScanExec => toMetric(i.metrics) ++
+        toMetric(i.driverMetrics) // not sure why i.driverMetrics not included in i.metrics
+      case i: ProjectExec => toMetric(i.metrics) // always empty
+      case i: V2ExistingTableWriteExec => toMetric(i.metrics) // TODO investigate, strange
+      case i: ExecutedCommandExec => toMetric(i.metrics) // always empty
+      case i: SerializeFromObjectExec => toMetric(i.metrics) // always empty
+      case i: MapElementsExec => toMetric(i.metrics) // always empty
+      case i: InputAdapter => toMetric(i.metrics) // always empty
+      case i: DeserializeToObjectExec => toMetric(i.metrics) // always empty
+      case i: MapPartitionsExec => toMetric(i.metrics) // always empty
+      case i if i.metrics.nonEmpty => toMetric(i.metrics)
+      case i => throw new IllegalStateException(s"Unsupported ${i.getClass.getName}: no metrics reported")
     }
-    node ++ p.children.map(resolvePlan).reduceOption(_ ++ _).getOrElse(Map.empty[Long, Long])
+    node ++ p.children.map(planToMetrics).reduceOption(_ ++ _).getOrElse(Empty)
   }
 
   private def toMetric(m: Map[String, SQLMetric]): Map[Long, Long] = {
