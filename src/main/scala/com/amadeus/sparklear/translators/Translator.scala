c9a816b8d3c04c2e5e626279710ec8c783ae32c8
Mauricio Jost <mauricio.jost@amadeus.com>
2023-12-05 15:09:34 +0100

    Add join test

--- src/main/scala/com/amadeus/sparklear/translators/Translator.scala
+++ src/main/scala/com/amadeus/sparklear/translators/Translator.scala
@@ -8,7 +8,11 @@ trait Translator[I <: PreReport, R <: Report] {
   def toAllReports(c: Config, p: I): Seq[R]
   def toReports(c: Config, p: I): Seq[R] = {
     val rep = toAllReports(c, p)
-    val frep = rep.filter(r => c.glasses.forall(g => g.eligible(r)))
+    val frep = if (c.glasses.isEmpty) { // no glasses? return all
+      rep
+    } else {
+      rep.filter(r => c.glasses.exists(g => g.eligible(r))) // glasses? filter
+    }
     frep
   }
   def toStringReports(c: Config, p: I): Seq[Translator.StringReport] =
--- src/test/scala/com/amadeus/sparklear/ReadCsvToDeltaSpec.scala
+++ src/test/scala/com/amadeus/sparklear/ReadCsvToDeltaSpec.scala
@@ -1,13 +1,11 @@
 package com.amadeus.sparklear
 
-import com.amadeus.sparklear.prereports.PreReport
 import com.amadeus.sparklear.reports.SqlPlanNodeReport
 import com.amadeus.sparklear.reports.glasses.SqlNodeGlass
 import com.amadeus.testfwk._
 import io.delta.tables.DeltaTable
 
 import java.nio.file.Path
-import scala.collection.mutable.ListBuffer
 
 class ReadCsvToDeltaSpec
     extends SimpleSpec
@@ -33,6 +31,7 @@ class ReadCsvToDeltaSpec
 
         describe("reading a delta table filtering and writing to another delta") {
           withAllSinks { sinks =>
+            // DF TABLE: iata_code, icao_code, ..., name, ..., country_name, country_code, ...
             val df = DeltaTable.forPath(subdir(tmpDir, "deltadir1")).toDF
             val cfg = defaultTestConfig.withOnlySqlEnabled
               .withGlasses(
@@ -40,6 +39,12 @@ class ReadCsvToDeltaSpec
                   SqlNodeGlass(
                     nodeNameRegex = Some(".*Filter.*"),
                     parentNodeNameRegex = Some(".*WholeStageCodegen.*")
+                  ),
+                  SqlNodeGlass(
+                    nodeNameRegex = Some(".*Scan.*")
+                  ),
+                  SqlNodeGlass(
+                    nodeNameRegex = Some(".*Join.*")
                   )
                 )
               )
@@ -47,29 +52,45 @@ class ReadCsvToDeltaSpec
             val eventsListener = new SparklEar(cfg)
             spark.sparkContext.addSparkListener(eventsListener)
 
-            spark.sparkContext.setJobDescription("job1")
-            val dfFiltered1 = df.filter(df("iata_code") === "EZE") // 1 record matching
-            dfFiltered1.write.format("delta").mode("overwrite").save(subdir(tmpDir, "deltadirjob1"))
-
-            spark.sparkContext.setJobDescription("job2")
-            val dfFiltered2 =
+            spark.sparkContext.setJobDescription("jobfilter")
+            val dfFiltered1 =
               df.filter(df("adm1_name_ascii") === "Cordoba" && df("fcode") === "AIRP") // 16 records matching
-            dfFiltered2.write.format("delta").mode("overwrite").save(subdir(tmpDir, "deltadirjob2"))
+            dfFiltered1.write.format("delta").mode("overwrite").save(subdir(tmpDir, "jobfilterdir"))
 
-            spark.sparkContext.removeSparkListener(eventsListener)
-            it("should report the filter plan node resulting in 1 record") {
+            // LOOKUP TABLE: country_code, country name
+            spark.sparkContext.setJobDescription("joblookuptable")
+            df
+              .select("country_code", "country_name")
+              .distinct()
+              .write
+              .format("delta")
+              .mode("overwrite")
+              .save(subdir(tmpDir, "joblookuptabledir"))
+
+            it("should report the filter plan nodes") {
               val actual = sinks.reports
                 .collect { case i: SqlPlanNodeReport => i }
-                .filter(_.jobName == "job1")
-                .flatMap(_.metrics)
-              actual should equal(Seq(("number of output rows", "1")))
+                .filter(_.nodeName.contains("Filter"))
+                .filter(i => i.jobName.contains("jobfilter"))
+                .map(i => (i.jobName, i.metrics))
+              actual should equal(Seq(("jobfilter", Seq(("number of output rows", "16")))))
             }
-            it("should report the filter plan node resulting in 16 records") {
+            spark.sparkContext.setJobDescription("jobjoin")
+            val df3 = df.select("name", "country_code").filter(df("iata_code") === "COR").as("l")
+              .join(DeltaTable.forPath(subdir(tmpDir, "joblookuptabledir")).toDF.as("r"), "country_code")
+
+            df3.write.format("delta").mode("overwrite").save(subdir(tmpDir, "deltadirjob3"))
+            it("should report the join plan node") {
               val actual = sinks.reports
                 .collect { case i: SqlPlanNodeReport => i }
-                .filter(_.jobName == "job2")
-                .flatMap(_.metrics)
-              actual should equal(Seq(("number of output rows", "16")))
+                .filter(_.nodeName.contains("BroadcastHashJoin"))
+                .filter(i => i.jobName.contains("jobjoin"))
+                .map(i => (i.jobName, i.metrics))
+              actual should equal(
+                Seq(
+                  ("jobjoin", Seq(("number of output rows", "2")))
+                )
+              )
             }
           }
         }
