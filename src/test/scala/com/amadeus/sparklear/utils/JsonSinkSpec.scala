0a02d05e8d3d52aa3d9726c3e509a266bed7aa75
pbjouber_amadeus <bruno.joubert@amadeus.com>
2025-07-23 14:23:10 +0200

    Update JsonSinkSpec

--- src/test/scala/com/amadeus/sparklear/utils/JsonSinkSpec.scala
+++ src/test/scala/com/amadeus/sparklear/utils/JsonSinkSpec.scala
@@ -4,28 +4,23 @@ import com.amadeus.sparklear.JsonSink
 import com.amadeus.sparklear.reports.JobReport
 import com.amadeus.testfwk.{SimpleSpec, SinkSupport, TempDirSupport}
 
+import java.io.File
 import java.time.Instant
 
 class JsonSinkSpec extends SimpleSpec with TempDirSupport with SinkSupport {
   describe("json sink") {
     it("should write job reports") {
       withTmpDir { tmpDir =>
-        import java.io.File
-        val folder = new File(s"$tmpDir/json-sink")
-        if (!folder.exists()) {
-          folder.mkdirs()
-        }
-
         val sparkApplicationId: String = java.util.UUID.randomUUID().toString
         val jsonSink = new JsonSink(
           sparkApplicationId = sparkApplicationId,
-          destination = s"$tmpDir/json-sink",
+          destination = s"$tmpDir",
           writeBatchSize = 1)
 
         val jr = JobReport(1, "testgroup", "testjob", Instant.now.getEpochSecond, 1000, "1", Seq(1))
         jsonSink.sink(jr)
 
-        val jsonFile = new File(s"$tmpDir/json-sink/$sparkApplicationId/job-reports.json")
+        val jsonFile = new File(s"$tmpDir/$sparkApplicationId/job-reports.json")
         jsonFile.length() should not equal(0)
       }
     }
