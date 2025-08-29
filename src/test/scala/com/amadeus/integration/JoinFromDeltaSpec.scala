a8ba94134a1eda7717c2c2997b73705f038f0d39
Generoso Pagano <generoso.pagano@amadeus.com>
2025-08-29 19:20:27 +0200

    Split 2 functional cases in 2 separate tests to avoid having 2 describe() in parallel

---
+++ src/test/scala/com/amadeus/integration/JoinFromDeltaSpec.scala
@@ -0,0 +1,92 @@
+package com.amadeus.integration
+
+import com.amadeus.sparklear.SparklEar
+import com.amadeus.sparklear.reports.SqlReport
+import com.amadeus.testfwk._
+import com.amadeus.testfwk.filters.SqlNodeFilter
+import io.delta.tables.DeltaTable
+
+import java.nio.file.Path
+
+class JoinFromDeltaSpec
+    extends SimpleSpec
+    with SparkSupport
+    with OptdSupport
+    with JsonSupport
+    with ConfigSupport
+    with TempDirSupport
+    with SinkSupport {
+  val DeltaSettings = List(
+    ("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension"),
+    ("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog"),
+    ("spark.sql.adaptive.enabled", "false")
+  )
+
+  private def subdir(base: Path, s: String) = base.resolve(s).toAbsolutePath.toFile.toString
+
+  describe("The listener when joining two dataframes read from delta") {
+    withSpark(DeltaSettings, appName = this.getClass.getName) { spark =>
+      withTmpDir { tmpDir =>
+        val df = readOptd(spark)
+        df.write.format("delta").mode("overwrite").save(subdir(tmpDir, "deltadir1"))
+
+        withTestableSink { sinks =>
+          // DF TABLE: iata_code, icao_code, ..., name, ..., country_name, country_code, ...
+          val df = DeltaTable.forPath(subdir(tmpDir, "deltadir1")).toDF
+          val cfg = defaultTestConfig.withOnlySqlEnabled.withSink(sinks)
+
+          val eventsListener = new SparklEar(cfg)
+          spark.sparkContext.addSparkListener(eventsListener)
+
+          // Create a lookup table with country_code and country_name, containing 252 rows
+          spark.sparkContext.setJobDescription("joblookuptable")
+          df
+            .select("country_code", "country_name")
+            .distinct()
+            .coalesce(1) // to have only one file
+            .write
+            .format("delta")
+            .mode("overwrite")
+            .save(subdir(tmpDir, "joblookuptabledir"))
+
+          // JOIN with the lookup table
+          spark.sparkContext.setJobDescription("jobjoin")
+          val df3 = df
+            .select("name", "country_code")
+            .filter(df("iata_code") === "COR")
+            .as("l")
+            .join(DeltaTable.forPath(subdir(tmpDir, "joblookuptabledir")).toDF.as("r"), "country_code")
+          df3.write.format("delta").mode("overwrite").save(subdir(tmpDir, "deltadirjob3"))
+
+          it("should report the two scan parquet nodes: build side and probe side of the join") {
+            val f = SqlNodeFilter(
+              nodeNameRegex = Some(".*Scan parquet.*"),
+              jobNameRegex = Some("jobjoin"),
+              isLeaf = Some(true)
+            )
+            val actual = sinks.reports
+              .collect { case i: SqlReport => i.nodes }
+              .flatten
+              .filter(r => f.eligible(r))
+              .map(i => (i.jobName, i.metrics("number of files read"), i.metrics("number of output rows")))
+            actual.size should equal(2) // one for the build side and one for the probe side
+            actual should contain(("jobjoin", "1", "252")) // lookup table scan (probe side)
+          }
+
+          it("should report the join plan node") {
+            val f = SqlNodeFilter(
+              nodeNameRegex = Some(".*Join.*"),
+              jobNameRegex = Some("jobjoin")
+            )
+            val actual = sinks.reports
+              .collect { case i: SqlReport => i.nodes }
+              .flatten
+              .filter(r => f.eligible(r))
+              .map(i => (i.jobName, i.metrics))
+            actual should equal(Seq(("jobjoin", Map("number of output rows" -> "2"))))
+          }
+        }
+      }
+    }
+  }
+}
