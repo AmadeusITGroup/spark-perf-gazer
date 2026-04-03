6f648e76a037febae80ce7b46aafdf800c7c869f
Mauricio Jost <mauricio.jost@amadeus.com>
2026-04-03 11:24:13 +0200

    perfGazer.getSnippets returns all snippets

--- core/src/main/scala/com/amadeus/perfgazer/PerfGazer.scala
+++ core/src/main/scala/com/amadeus/perfgazer/PerfGazer.scala
@@ -152,8 +152,8 @@ class PerfGazer(val c: PerfGazerConfig, val sink: Sink) extends SparkListener {
   /**
     * Expose sink and snippets for external usage (e.g., Databricks Notebooks).
     */
-  def getSnippets: String = {
-    sink.generateAllViewSnippets().mkString("\n")
+  def getSnippets: Set[String] = {
+    sink.generateAllViewSnippets()
   }
 }
 
